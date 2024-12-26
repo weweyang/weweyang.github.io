7fa1e5d35e910b78af8a34fe92d2b93c9c02b37f
text <2473555802@qq.com>
2024-12-26 17:08:11 +0800

    Update love.js

--- css/love.js
+++ css/love.js
@@ -245,7 +245,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("   云宝  ", 23, 10);
+            ctx.fillText("   小庄  ", 23, 10);
             ctx.restore();
         },
         clear: function() {
